ad5ea23fee72b4bf3758444c1eee2c84596d5cae
pedro-icaro <icaropedro751@gmail.com>
2026-05-29 08:31:28 -0300

    refactor: Melhorando Reactplayer

--- src/app/player/[courseId]/[classId]/page.tsx
+++ src/app/player/[courseId]/[classId]/page.tsx
@@ -14,7 +14,7 @@ export default async function PagePlayer({ params }: Props) {
 
   return (
     <main className="flex flex-col gap-2 h-[calc(100vh-64px)]">
-      <div className="flex gap-2 h-full">
+      <div className="flex gap-8 h-full">
        <div className="flex-1 max-w-[480] h-[600]"> 
           <PlayerPlaylist 
             PlayCourseId={courseId}
@@ -27,22 +27,22 @@ export default async function PagePlayer({ params }: Props) {
                   {
                     done: true,
                     classId: "aula-01",
-                    title: "Curso de HTML5 - 00 - Site Completo - by Gustavo Guanabara",
+                    title: "1-Curso de HTML5 - 00 - Site Completo - by Gustavo Guanabara",
                   },
                   {
                     done: true,
                     classId:"aula-02",
-                    title: "Curso de HTML5 - 01 - Site Completo - by Gustavo Guanabara",
+                    title: "2-Curso de HTML5 - 01 - Site Completo - by Gustavo Guanabara",
                   },
                   {
                     done: false,
                   classId:"aula-03",
-                    title: "Curso de HTML5 - 02 - Site Completo - by Gustavo Guanabara",
+                    title: "3-Curso de HTML5 - 02 - Site Completo - by Gustavo Guanabara",
                   },
                   {
                     done: false,
                     classId:"aula-04",
-                    title: "Curso de HTML5 - 03 - Site Completo - by Gustavo Guanabara",
+                    title: "4-Curso de HTML5 - 03 - Site Completo - by Gustavo Guanabara",
                   },
                 ]
               },              { 
@@ -51,22 +51,22 @@ export default async function PagePlayer({ params }: Props) {
                   {
                     done: false,
                     classId:"aula-05",
-                    title: "Curso de HTML5 - 00 - Site Completo - by Gustavo Guanabara",
+                    title: "5-Curso de HTML5 - 00 - Site Completo - by Gustavo Guanabara",
                   },
                   {
                     done: false,
                     classId:"aula-06",
-                    title: "Curso de HTML5 - 01 - Site Completo - by Gustavo Guanabara",
+                    title: "6-Curso de HTML5 - 01 - Site Completo - by Gustavo Guanabara",
                   },
                   {
                     done: false,
                   classId:"aula-07",
-                    title: "Curso de HTML5 - 02 - Site Completo - by Gustavo Guanabara",
+                    title: "7-Curso de HTML5 - 02 - Site Completo - by Gustavo Guanabara",
                   },
                   {
                     done: false,
                     classId:"aula-08",
-                    title: "Curso de HTML5 - 03 - Site Completo - by Gustavo Guanabara",
+                    title: "8-Curso de HTML5 - 03 - Site Completo - by Gustavo Guanabara",
                   },
                 ]
               }, { 
@@ -76,22 +76,22 @@ export default async function PagePlayer({ params }: Props) {
                   {
                     done: true,
                     classId: "aula-01",
-                    title: "Curso de HTML5 - 00 - Site Completo - by Gustavo Guanabara",
+                    title: "9-Curso de HTML5 - 00 - Site Completo - by Gustavo Guanabara",
                   },
                   {
                     done: true,
                     classId:"aula-02",
-                    title: "Curso de HTML5 - 01 - Site Completo - by Gustavo Guanabara",
+                    title: "10-Curso de HTML5 - 01 - Site Completo - by Gustavo Guanabara",
                   },
                   {
                     done: false,
                   classId:"aula-03",
-                    title: "Curso de HTML5 - 02 - Site Completo - by Gustavo Guanabara",
+                    title: "11-Curso de HTML5 - 02 - Site Completo - by Gustavo Guanabara",
                   },
                   {
                     done: false,
                     classId:"aula-04",
-                    title: "Curso de HTML5 - 03 - Site Completo - by Gustavo Guanabara",
+                    title: "12-Curso de HTML5 - 03 - Site Completo - by Gustavo Guanabara",
                   },
                 ]
               },  
--- src/components/player/PlayerVideoPlayer.tsx
+++ src/components/player/PlayerVideoPlayer.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import ReactPlayer from "react-player"
+import dynamic from "next/dynamic";
+
+const ReactPlayer = dynamic(() => import("react-player"), {ssr: false});
 
 interface PropsVideoPlayer{
     videoId:string;
 }
 
-export default function PlayerVideoPlayer({videoId}:PropsVideoPlayer){
+export default function PlayerVideoPlayer({videoId}:PropsVideoPlayer){    
     return(
         <>
             <ReactPlayer src={`https://www.youtube.com/watch?v=${videoId}`} height={400} width={700}/>
