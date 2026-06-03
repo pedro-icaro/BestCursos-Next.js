314142dd0a47e723d1e789ab2950b81635d017cd
pedro-icaro <icaropedro751@gmail.com>
2026-06-03 20:00:30 -0300

    feat: Adicionado cache

--- src/shared/services/api-youtube/index.ts
+++ src/shared/services/api-youtube/index.ts
@@ -1,8 +1,16 @@
 import { youtube } from "@googleapis/youtube";
 
+
+const fetchWithNextConfig = (nextConfig?: NextFetchRequestConfig): typeof fetch =>{
+    return (input, params = {}) => {
+        return fetch(input, { ...params, next: nextConfig})
+    };
+};
+
 const YoutubeAPIClient = youtube({
     version: "v3",
     auth: process.env.YOUTUBE_API_KEY,
+    fetchImplementation: fetchWithNextConfig(),
 });
 
 export const APIYoutube = {
@@ -13,7 +21,7 @@ export const APIYoutube = {
             const { data } = await YoutubeAPIClient.playlists.list({
                 part: ["snippet"],
                 id: playlistIds, 
-            });
+            },{ fetchImplementation: fetchWithNextConfig({ revalidate: (60 * 60) * 96})});
 
             const courses = (data.items || []).map(item => ({
                 id: item.id || "",
