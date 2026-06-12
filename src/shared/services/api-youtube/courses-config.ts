753f783b83aaa5c483ad2703fb0db17b57576372
pedro-icaro <icaropedro751@gmail.com>
2026-06-12 20:05:10 -0300

    feat: Adicionando mais conteudo de cursos a plataforma

--- src/shared/services/api-youtube/courses-config.ts
+++ src/shared/services/api-youtube/courses-config.ts
@@ -2,5 +2,11 @@ export const PLAYLIST_IDS = [
   "PLHz_AreHm4dlAnJ_jJtV29RFxnPHDuk9o", 
   "PLHz_AreHm4dlsK3Nr9GVvXCbpQyHQl1o1", 
   "PL29TaWXah3iYzP5FGywXezXm4ZvBibGSk",
-  "PLHz_AreHm4dkZ9-atkcmcBaMZdmLHft8n"
+  "PLHz_AreHm4dkZ9-atkcmcBaMZdmLHft8n",
+  "PL2Fdisxwzt_d89X6NkB8C2VroMKXsn6V7",
+  "PL2Fdisxwzt_cHa9G_K5dfwxBVy8xnzM35",
+  "PLGFwuV_B-U2W263p_tKmLo51O32sCdtiC",
+  "PLK_SnY7SMXG64RDi_4QFAeyaTgwxPBo5Q",
+  "PLHz_AreHm4dmSj0MHol_aoNYCSGFqvfXV",
+  "PLHz_AreHm4dm7ZULPAmadvNhH6vk9oNZA"
 ];
